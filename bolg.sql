dc144aa55b14dc1b49f105137dd4318e046f1054
hansiming <csdhsm@163.com>
2017-03-04 16:06:51 +0800

    update sql

--- bolg.sql
+++ bolg.sql
@@ -5,9 +5,9 @@ CREATE TABLE auth_roles (id INT PRIMARY KEY AUTO_INCREMENT, role_name VARCHAR(25
 
 ALTER TABLE users ADD CONSTRAINT fk_users_auth_roles_role_id FOREIGN KEY (role_id) REFERENCES auth_roles(id);
 
-CREATE TABLE blogs (id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(1047) NOT NULL, content TEXT NOT NULL, create_time DATETIME NOT NULL,
+CREATE TABLE blogs (id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(1047) NOT NULL, content TEXT NOT NULL, create_time DATE NOT NULL,
  comment_count INT DEFAULT 0 COMMENT '评论数', upvote_count INT DEFAULT 0 COMMENT '点赞数', watch_count INT DEFAULT 0 COMMENT '查看数',
- lable VARCHAR(255) COMMENT '标签，存储形式 aaa,bbb,ccc', user_id INT NOT NULL);
+ lable VARCHAR(255) COMMENT '标签，存储形式 aaa,bbb,ccc', recommend INT DEFAULT 0 COMMENT '推荐权值，越大越往前', user_id INT NOT NULL);
  
  ALTER TABLE blogs ADD CONSTRAINT fk_blogs_users_user_id FOREIGN KEY (user_id) REFERENCES users(id);
  
